96ffe31a95e837be318bcff9b7093cdf25a7a456
Robert S <rseifert.phone@gmail.com>
2015-01-24 11:25:08 -0500

    Added recipe for reinforced casing

--- src/main/java/com/builtbroken/militarybasedecor/vanilla/VanillaModule.java
+++ src/main/java/com/builtbroken/militarybasedecor/vanilla/VanillaModule.java
@@ -3,7 +3,11 @@ package com.builtbroken.militarybasedecor.vanilla;
 import com.builtbroken.mc.lib.mod.loadable.ILoadable;
 import com.builtbroken.militarybasedecor.MilitaryBaseDecor;
 import com.builtbroken.militarybasedecor.vanilla.content.*;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 /**
  * Content themed to vanilla MC rather than any set mod or History era
@@ -35,6 +39,7 @@ public class VanillaModule implements ILoadable
     @Override
     public void postInit()
     {
-
+        //Reinforced wood casing
+        GameRegistry.addShapedRecipe(new ItemStack(reinforcedCasing, 1, 0), new Object[]{"SWS", "WSW" , "SWS", 'S', Items.stick, 'W', Blocks.planks});
     }
 }
