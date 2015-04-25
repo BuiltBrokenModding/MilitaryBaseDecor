e7d4a1062c20ff4d53367ef2a1c402401462ee55
Henry Stabell <hennamann9999@gmail.com>
2015-04-25 15:51:45 +0200

    added new rusty textures

--- src/main/java/com/builtbroken/militarybasedecor/vanilla/content/block/BlockRustyIronBlock.java
+++ src/main/java/com/builtbroken/militarybasedecor/vanilla/content/block/BlockRustyIronBlock.java
@@ -3,6 +3,7 @@ package com.builtbroken.militarybasedecor.vanilla.content.block;
 import com.builtbroken.militarybasedecor.MilitaryBaseDecor;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 
 public class BlockRustyIronBlock extends Block {
@@ -15,4 +16,9 @@ public class BlockRustyIronBlock extends Block {
 		this.setBlockName("rusty_iron_block");
 		this.setBlockTextureName(MilitaryBaseDecor.DOMAIN + ":" + "rusty_iron_block");
 	}
+	
+	public MapColor getMapColor(int p_149728_1_)
+    {
+        return MapColor.adobeColor;
+    }
 }
